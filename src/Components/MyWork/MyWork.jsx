f23527471b5530be7a888d0cb97049c7da70aea9
ihjasdev <ihjasbinabdullah@gmail.com>
2024-08-06 11:11:40 +0530

    ⚡️improving the mywork component

---
+++ src/Components/MyWork/MyWork.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./MyWork.css";
+import theme_pattern from "../../assets/pattern_theme2.svg";
+import Mywork_data from "../../assets/mywork_data";
+import arrow_img from "../../assets/arrow_img.svg";
+
+const MyWork = () => {
+  return (
+    <div className="mywork">
+      <div className="mywork-tittle">
+        <h1>My latest work</h1>
+        <img src={theme_pattern} alt="" />
+      </div>
+      <div className="mywork-container">
+        {Mywork_data.map((work, index) => {
+          return <img key={index} src={work.w_img} alt="" />
+        })}
+      </div>
+      <div className="mywork-showmore">
+        <p>Show More</p>
+        <img src={arrow_img} alt=""/>
+      </div>
+    </div>
+  );
+};
+
+export default MyWork;
---
+++ src/assets/mywork_data.js
@@ -0,0 +1,34 @@
+import w1 from "../assets/images/w1.png"
+import w2 from "../assets/images/w2.png"
+import w3 from "../assets/images/w3.png"
+import w4 from "../assets/images/w4.png"
+
+
+const mywork_data = [
+    {
+        w_no: 1,
+        w_name: "web design",
+        w_img: w1
+    },
+    {
+        w_no: 2,
+        w_name: "web development",
+        w_img: w2
+    },
+    {
+        w_no: 3,
+        w_name: "seo optimization",
+        w_img: w3
+    },
+    {
+        w_no: 4,
+        w_name: "content creation",
+        w_img: w4
+    },
+    {
+        w_no: 5,
+        w_name: "graphic design",
+        w_img: w1
+    }
+]
+export default mywork_data;
